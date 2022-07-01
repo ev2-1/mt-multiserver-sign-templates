664144df5dac4e1da972fe002feebcb2a12b52bd
riley <riley@e926.de>
2022-07-01 21:43:28 +0200

    basic idea

---
+++ initializeTemplates.go
@@ -0,0 +1,63 @@
+package signTemplates
+
+import (
+	"github.com/ev2-1/mt-multiserver-signs"
+
+	"errors"
+	"sync"
+)
+
+var initTemplatesMu sync.Once
+
+func initTemplates() {
+	initTemplatesMu.Do(func() {
+		registerTemplate(Text, func(pos signs.SignPos, a ...string) (error, *signs.Sign) {
+			if len(a) != 1 {
+				return errors.New("Not enough arguments to Template Text"), nil
+			}
+
+			return nil, &signs.Sign{
+				Pos: &pos,
+
+				Text:  "%s",
+				Color: "black",
+				Dyn: []signs.DynContent{
+					&signs.Text{
+						Text: a[0],
+					},
+				},
+			}
+		})
+
+		registerTemplate(ServerPlayerCnt, func(pos signs.SignPos, a ...string) (error, *signs.Sign) {
+			if len(a) != 1 {
+				return errors.New("Not enough arguments to Template ServerPlayerCnt (Arguments: Server)"), nil
+			}
+
+			return nil, &signs.Sign{
+				Pos: &pos,
+
+				Text:  "X-------------X\n|%s|\n|%s|\nX-------------X",
+				Color: "black",
+				Dyn: []signs.DynContent{
+					&signs.Center{
+						Filler: ' ',
+						Length: 14,
+
+						Content: &signs.Text{
+							Text: a[0],
+						},
+					},
+					&signs.Center{
+						Filler: ' ',
+						Length: 14,
+
+						Content: &signs.PlayerCnt{
+							Srv: a[0],
+						},
+					},
+				},
+			}
+		})
+	})
+}
---
+++ template.go
@@ -0,0 +1,48 @@
+package signTemplates
+
+import (
+	"github.com/ev2-1/mt-multiserver-signs"
+
+	"sync"
+)
+
+type TemplateID uint16
+
+const (
+	Text            TemplateID = iota // insert thing here:
+	ServerPlayerCnt                   // not after this
+)
+
+// templates returns a list of templates
+func Templates() (m map[TemplateID]string) {
+	initTemplates()
+
+	m = make(map[TemplateID]string)
+
+	for k := TemplateID(0); k < ServerPlayerCnt; k++ {
+		m[k] = k.String()
+	}
+
+	return
+}
+
+type Template func(pos signs.SignPos, args ...string) (error, *signs.Sign)
+
+var templates map[TemplateID]Template
+var templatesMu sync.RWMutex
+
+func registerTemplate(id TemplateID, t Template) {
+	templatesMu.Lock()
+	defer templatesMu.Unlock()
+
+	templates[id] = t
+}
+
+func GetTemplate(t TemplateID) Template {
+	initTemplates()
+
+	templatesMu.RLock()
+	defer templatesMu.RUnlock()
+
+	return templates[t]
+}
